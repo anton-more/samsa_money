47e9802f01b41b3f1414488179385de655ef1cba
anton-more <itskovych.more@gmail.com>
2024-05-24 00:08:25 +0300

    Add requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-import telebot
-
-bot = telebot.TeleBot("7009129559:AAF4Ai_u6deROWCW7vDjK70U3eJsf7h1fRo")
-
-
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, 'салам!')
-
-
-bot.polling(none_stop=True)
+Procfile
